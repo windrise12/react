5bd2d403e3c65edd3beb4d3e9fba471e3c37ac49
windrise12 <1157964745@qq.com>
2020-04-13 00:42:08 +0800

    add1

---
+++ react/public/template/precache-manifest.30b880dd97f61c825c10482e43cb81a2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d26fb503fe01c06ddcfc51631c5901a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2791d3c5339709c2d4",
+    "url": "/static/css/main.777528e0.chunk.css"
+  },
+  {
+    "revision": "e30752fd9a009b41f69a",
+    "url": "/static/js/2.382fc36a.chunk.js"
+  },
+  {
+    "revision": "90fb4deb912422b947a6490378160cce",
+    "url": "/static/js/2.382fc36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2791d3c5339709c2d4",
+    "url": "/static/js/main.799f14da.chunk.js"
+  },
+  {
+    "revision": "508fd98b24efb6d63c73",
+    "url": "/static/js/runtime-main.5fcbc9fa.js"
+  },
+  {
+    "revision": "bc90926b0b8d90af8261ba51c59e8322",
+    "url": "/static/media/find1.bc90926b.png"
+  },
+  {
+    "revision": "75d9432341e403bea72f3b197cc37aed",
+    "url": "/static/media/find3.75d94323.png"
+  },
+  {
+    "revision": "18f5cefceabd2841ef46b95980d895ec",
+    "url": "/static/media/find4.18f5cefc.png"
+  },
+  {
+    "revision": "32f502e0489923206a691612d353caac",
+    "url": "/static/media/logo.32f502e0.png"
+  },
+  {
+    "revision": "25edb6fc05cf3c3fb47e600e2a5988cb",
+    "url": "/static/media/logo1.25edb6fc.png"
+  },
+  {
+    "revision": "4dc96b0a8c8db0996cc18de4417c4f66",
+    "url": "/static/media/weichat.4dc96b0a.png"
+  }
+]);
